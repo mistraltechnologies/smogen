27e84fa038d950958039ef10d18c321e0c7e42a7
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-11-02 17:43:24 +0000

    Update tests for generating into existing and non-existing packages.

--- test/com/mistraltech/smogen/AbstractGeneratorTest.java
+++ test/com/mistraltech/smogen/AbstractGeneratorTest.java
@@ -2,10 +2,12 @@ package com.mistraltech.smogen;
 
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import com.mistraltech.smogen.codegenerator.Generator;
 import com.mistraltech.smogen.codegenerator.matchergenerator.MatcherGeneratorProperties;
+import com.mistraltech.smogen.utils.ActionUtils;
 import com.mistraltech.smogen.utils.PsiUtils;
 import com.mistraltech.smogen.utils.SourceRootUtils;
 
@@ -13,25 +15,26 @@ import java.io.File;
 import java.util.List;
 
 public abstract class AbstractGeneratorTest extends LightCodeInsightFixtureTestCase {
-
     @Override
     protected String getTestDataPath() {
         return new File("").getAbsolutePath().replace(File.separatorChar, '/') + "/testData";
     }
 
     protected void doTest(String inputFilePath, String expectedGeneratedFilePath, MatcherGeneratorProperties generatorProperties) {
-        List<VirtualFile> sourceRoots = SourceRootUtils.getSourceAndTestSourceRoots(getProject());
-
         final PsiFile sourceFile = myFixture.configureByFile(inputFilePath);
         final PsiClass sourceClass = PsiUtils.getClassFromFile(sourceFile);
 
-        generatorProperties.setSourceRoot(sourceRoots.get(0))
+        generatorProperties.setSourceRoot(getSourceRoot())
                 .setSourceClass(sourceClass);
 
         new Generator(generatorProperties).generate();
 
         final String generatedFile = generatorProperties.getPackageName().replace('.','/') + "/" + generatorProperties.getClassName() + ".java";
         myFixture.checkResultByFile(generatedFile, expectedGeneratedFilePath, false);
+    }
+
+    private VirtualFile getSourceRoot() {
+        return SourceRootUtils.getSourceAndTestSourceRoots(getProject()).get(0);
     }
 
     protected MatcherGeneratorProperties generatorProperties() {
@@ -48,5 +51,16 @@ public abstract class AbstractGeneratorTest extends LightCodeInsightFixtureTestC
         String inputFilePath = testName + "/" + "input.java";
         String generatedFilePath = testName + "/" + "generated.java";
         doTest(inputFilePath, generatedFilePath, generatorProperties);
+    }
+
+    protected void createPackage(final String packageName) {
+        final PsiDirectory baseDirectory = myFixture.getPsiManager().findDirectory(getSourceRoot());
+        assert baseDirectory != null;
+        ActionUtils.runAction(new Runnable() {
+            @Override
+            public void run() {
+                PsiUtils.createMissingDirectoriesForPackage(baseDirectory, packageName);
+            }
+        });
     }
 }
--- test/com/mistraltech/smogen/GeneratorTest.java
+++ test/com/mistraltech/smogen/GeneratorTest.java
@@ -1,20 +1,28 @@
 package com.mistraltech.smogen;
 
+import com.mistraltech.smogen.utils.PsiUtils;
+
 public class GeneratorTest extends AbstractGeneratorTest {
 
-  public void testMinimalClass() {
-    doTest("minimal_class", generatorProperties());
-  }
+    public void testMinimalClass() {
+        doTest("minimal_class", generatorProperties());
+    }
 
-  public void testSimpleProperty() {
-    doTest("simple_property", generatorProperties());
-  }
+    public void testSimpleProperty() {
+        doTest("simple_property", generatorProperties());
+    }
 
-  public void testBasicExtensibleMatcher() {
-    doTest("basic_extensible", generatorProperties().setExtensible(true));
-  }
+    public void testBasicExtensibleMatcher() {
+        doTest("basic_extensible", generatorProperties().setExtensible(true));
+    }
 
-  public void testGenerateIntoNonDefaultPackage() {
-    doTest("non_default_package", generatorProperties().setPackageName("com.acme"));
-  }
+    public void testGenerateIntoNonExistentNonDefaultPackage() {
+        doTest("non_default_package", generatorProperties().setPackageName("com.acme"));
+    }
+
+    public void testGenerateIntoExistingNonDefaultPackage() {
+        createPackage("com.acme");
+        doTest("non_default_package", generatorProperties().setPackageName("com.acme"));
+    }
+
 }
